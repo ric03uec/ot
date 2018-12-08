0228bdbeaa7331c0729cef147d858b3807eab433
Devashish <devashish.86@gmail.com>
2018-12-07 18:19:30 -0800

    fixing pointers

--- golang/cleanupImages/images.go
+++ golang/cleanupImages/images.go
@@ -8,21 +8,31 @@ import (
 )
 
 type GCP struct {
-	client storage.Client
+	// store the address of a context object
+	ctx		*context.Context
+
+	// store the address of the Client
+	client	*storage.Client
+
+	projectId string
 }
 
 func NewGCPClient(projectId string) (*GCP, error){
-	// connect and validate creds
+	// returns a non-nil, empty Context object/struct
 	ctx := context.Background()
-
 	log.Printf("Creating bucket context")
+
+	// returns a pointer to a Client
 	client, err := storage.NewClient(ctx)
 	if err != nil {
 		log.Fatalf("Failed to create client: %v", err)
 	}
 
+	// store the address of the context
 	gcpClient := &GCP{
-		client:		*client,
+		ctx:		&ctx,
+		client:		client,
+		projectId:	projectId,
 	}
 
 	return gcpClient, nil
@@ -31,15 +41,16 @@ func NewGCPClient(projectId string) (*GCP, error){
 func (gcp *GCP) GetInventory() {
 	log.Printf("geting inventory")
 
-// 	bucketName := "my-new-bucket-ric03uec"
-// 	// create a bucket instance
-// 	bucket := gcp.client.Bucket(bucketName)
-//
-// 	if err := bucket.Create(ctx, projectId, nil); err != nil {
-// 		log.Fatalf("Failed to ccreate bucket: %v", err)
-// 	}
-//
-// 	log.Printf("Bucket created: %v", bucketName)
+ 	bucketName := "my-new-bucket-ric03uec"
+	// create a bucket instance
+ 	bucket := *gcp.client.Bucket(bucketName)
+
+	// reference the address of the context
+ 	if err := bucket.Create(*gcp.ctx, gcp.projectId, nil); err != nil {
+ 		log.Fatalf("Failed to ccreate bucket: %v", err)
+ 	}
+
+ 	log.Printf("Bucket created: %v", bucketName)
 	// get
 	// get list of machine images
 	// get list of whitelisted images
